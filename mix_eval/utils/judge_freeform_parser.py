5a557704bcbd78df4d5784c6e271f5d7b52bd897
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2024-06-20 23:15:54 +0200

    Update judge_freeform_parser.py

--- mix_eval/utils/judge_freeform_parser.py
+++ mix_eval/utils/judge_freeform_parser.py
@@ -36,7 +36,7 @@ class ChatGPTJudgeCloseendFreeform:
     
     def _GPT_decode(self, inputs):
         completion = self.client.chat.completions.create(
-                            model=os.getenv('MODEL_PARSER_NAME'),,
+                            model=os.getenv('MODEL_PARSER_NAME'),
                             response_format={ "type": 'text'},
                             max_tokens=self.MAX_NEW_TOKENS,
                             messages=self.format_prompts(inputs),
